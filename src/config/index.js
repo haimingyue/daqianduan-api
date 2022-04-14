956d66c43cece7347f87437e03074b8e9746f96a
simoon <420526391@qq.com>
2022-04-14 17:39:53 +0800

    【配置修改】mongodb的密码修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
-const DB_URL = 'mongodb://test:d6905295@47.105.85.212:27017/testdb'
+const DB_URL = 'mongodb://test:123456@47.105.85.212:27017/testdb';
 const REDIS = {
-  host: '47.105.85.212',
-  port: 15001,
-  password: '123456'
-}
-const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
+	host: '47.105.85.212',
+	port: 15001,
+	password: '123456'
+};
+const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!';
 
 export default {
-  DB_URL,
-  REDIS,
-  JWT_SECRET
-}
+	DB_URL,
+	REDIS,
+	JWT_SECRET
+};
