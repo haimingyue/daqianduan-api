6d20cc7bea290b968d7b143cdc8f90085a5ad3ad
simoon <420526391@qq.com>
2022-04-14 18:08:48 +0800

    【功能新增】Eslint处理

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: 'eslint:recommended',
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+	},
+	parserOptions: {
+		sourceType: 'module',
+		parser: 'babel-eslint'
+	}
+};
